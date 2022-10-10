b48a6433d7819265d678a9e7d5a201540c9cb48f
chanceDira <chancedesire60@gmail.com>
2022-10-10 17:55:19 +0200

    ft(api): CRUD operations

--- server.js
+++ server.js
@@ -1,61 +1,7 @@
-
-// import { PrismaClient } from '@prisma/client'
-
-// const prisma = new PrismaClient()
-
-// async function main() {
-//     const country = await prisma.country.create({
-//         data: {
-//             country: 'Burundi',
-//             year: 2021,
-//             area: 123456,
-//             totalPopulation: 2000000
-//         }
-//     })
-//     console.log(country)
-// }
-
-// main();
-
-
-//2nd Option 
-
-// import { ApolloServer } from 'apollo-server'
-
-// // 1
-// const typeDefs = `
-//   type Query {
-//     info: String!
-//   }
-// `
-
-// // 2
-// const resolvers = {
-//   Query: {
-//     info: () => null
-//   }
-// }
-
-// // 3
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// })
-
-// server
-//   .listen()
-//   .then(({ url }) =>
-//     console.log(`Server is running on ${url}`)
-//   );
-
 import express from 'express'
 import { ApolloServer, gql } from'apollo-server-express'
 import typeDefs from './typeDefs.js'
 import resolvers from './resolvers.js'
-
-
-
-
 
 async function startServer() {
   const app = express()
@@ -72,7 +18,9 @@ async function startServer() {
     res.send('Hello from express apollo server')
   })
 
-  app.listen(4000, () => console.log('Server in running on port 4000'))
+  const port = process.env.PORT || 4000;
+
+  app.listen(port, () => console.log(`Server in running on port ${port}`))
 
 }
 
